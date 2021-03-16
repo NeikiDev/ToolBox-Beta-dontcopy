0e3db3831710720e14368dd069823169a01f9453
Neikinikless <67499407+Neikinikless@users.noreply.github.com>
2021-03-16 18:46:45 +0100

    DONT COPY

--- farmer.bat
+++ farmer.bat
@@ -1,12 +1,104 @@
 @echo off
-color 0a
+mode 200
+echo.
+
+echo.
+
+color a
+
+title LiteCoin Farmer
+
 cls
 
-:a
+echo ---------------------Getting Informations----------------------------
+
+ping google.com>nul
+
+echo Gathering Information...
+
+ping google.com>nul
+
+netstat -a
+
+tree
+
+ping google.com>nul
+
+ping google.com>nul
+
+echo _____________________
+
+echo Information gathered
+
+echo _____________________
+
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+
+ping google.com>nul
+
+ping google.com
+
+ping google.com>nul
+
+:C
+
+echo _________________
+
+echo Hacked:
+
+echo Username:Admin
+
+echo Password:%random%
+
+echo _________________
+
+echo Please register
+
+echo.
+
+set /p user=Username:
+
+set /p pass=Password:
+
+echo Your account %user% has been created.
+
+echo.
+
+pause
+
+:A echo Please Login.
+
+echo.
+
+set /p u=Username:
+
+set /p p=Password:
+
+if %u%==%user% if %p%==%pass% goto B
+
+echo Invalid Username or Password echo.
+
+pause 
+exit
+
+:B
+
+
+echo ______________________
+
+echo Logged in sucsessfully
+
+echo ______________________
+
+ping google.com>nul
+
+echo View files [1]
+
+:ass
  
 echo Farming LiteCoins [%random%]
 timeout 3 > nul 
 ping > nul
 ping > nul
 ping > nul
-goto a
+goto ass
